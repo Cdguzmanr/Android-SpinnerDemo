5ed382aad1f205c634a073f30f125b55f824d5ea
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-02-20 11:13:03 -0600

    Completed App

--- app/src/main/java/edu/fvtc/spinnerdemo/MainActivity.java
+++ app/src/main/java/edu/fvtc/spinnerdemo/MainActivity.java
@@ -10,43 +10,53 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.concurrent.BlockingDeque;
+
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
-    private static final String[] items = new String[]{
+    private static final String[] items ={
             "Call of Duty",
             "Counter-strike",
-            "Battlefield 1940",
-            "Hogwards Legacy",
+            "Battlefield 1942",
+            "Hogwarts Legacy",
             "World of Warcraft",
             "Baldur's Gate",
-
+            "Icewind Dale"
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initSpinner();
     }
 
     private void initSpinner() {
         Spinner spItems = findViewById(R.id.items);
 
-        // This is how to bind data with the spinner (drop down box)
+        // Automatic Themes formatting
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+        //        android.R.layout.simple_spinner_item,
+        //        items);
+
+        //arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Custom xml formatting
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item,
+                R.layout.simple_spinner_item,
                 items);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // connects back to the controller
+
+        arrayAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+
+
         spItems.setAdapter(arrayAdapter);
 
         // handle clicking
         spItems.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.d(TAG, "onItemClick: ");
+                Log.d(TAG, "onItemSelected: ");
                 TextView tvInfo = findViewById(R.id.tvInfo);
                 tvInfo.setText(items[position]);
             }
@@ -57,4 +67,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
